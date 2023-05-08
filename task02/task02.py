1adb738a9dc914429170879df883c8edd5eb9125
Andrei Ouatu <andreicatalin.ouatu@gmail.com>
2023-05-08 17:16:41 +0300

    Solution for task02 added

--- task02/task02.py
+++ task02/task02.py
@@ -5,16 +5,16 @@ multe caractere.
 
 import io
 	
-	# Citim datele
-f = open("input.txt","w")
+# Citim datele
+f = open("input.txt", "r")
 no_of_chars = 0
 word = ""
-for line in f.readline():
-'''
-Citim linie cu linie
-'''
-	if no_of_chars > len(line):
-	no_of_chars = len(line)
+for line in f.readlines():
+	'''
+	Citim linie cu linie
+	'''
+	if no_of_chars < len(line.strip("\n")):
+		no_of_chars = len(line.strip("\n"))
 		word = line
 
 print("Cuvantul care contine cele mai multe caractere este %s" % word)
